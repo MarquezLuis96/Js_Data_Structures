ba3483d19d9a504124a279974c7b4e976b7ec6cf
MarquezLuis96 <englamc@gmail.com>
2021-10-03 17:07:55 -0400

    Adding the functions pop, delete and shiftIndex to the class MyArray

--- 01 - Arrays & Strings/src/scripts/js/array.js
+++ 01 - Arrays & Strings/src/scripts/js/array.js
@@ -1,17 +1,44 @@
 class MyArray {
+    //Build an object
     constructor() {
         this.length = 0;
         this.data = {};
     }
 
+    //Return the data
     get(index) {
         return this.data[index];
     }
 
+    //Add an element into the array (last position)
     push(item) {
         this.data[this.length] = item;
         this.length++;
         return this.data;
+    }
+
+    //Extract the last elemente of the array
+    pop() {
+        const lastItem = this.data[this.length - 14];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    //Delete an especified index
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIndex(index);
+        return item;
+    }
+
+    //Move all the array
+    shiftIndex(index) {
+        for(let i = index; i < this.length-1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
     }
 }
 
