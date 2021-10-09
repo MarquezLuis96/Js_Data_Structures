261cb950bbee659bde147cc924d302f0ddc0895c
MarquezLuis96 <englamc@gmail.com>
2021-10-09 17:32:30 -0400

    Insert - Linked Lists Methods

--- 03 - Linked Lists/01 - Linked Lists/src/scripts/js/LinkedLists.js
+++ 03 - Linked Lists/01 - Linked Lists/src/scripts/js/LinkedLists.js
@@ -55,6 +55,34 @@ class MySinglyLinkedList {
 
         this.length++;
     }
+
+
+    getTheIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+
+
+    insert(index, value) {
+        if(index >= this.length) {
+            return this.append(value);
+        }
+
+        const newNode = new Node(value);
+        const firstPointer = this.getTheIndex(index - 1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+        return this;
+    }
 }
 
 
