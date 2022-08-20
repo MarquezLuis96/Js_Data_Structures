b4ad66bf22db614d4d7f1f00294070ef40d68f03
Luis Marquez <englamc@gmail.com>
2022-08-20 15:18:08 -0400

    Queues - Adding enqueue and peek methods

---
+++ 05 - Queues/src/scripts/js/Queues.js
@@ -0,0 +1,36 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    //This method adds a new element into the queue
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        }
+        else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        return this;
+    }
+
+    //Returns the first element of the queue
+    peek() {
+        return this.first;
+    }
+}
+
+const myQueue = new Queue();
