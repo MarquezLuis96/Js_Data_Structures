17f1c015d769eb1ec30524c5f7b817a60faedaf9
Luis Marquez <englamc@gmail.com>
2022-08-27 11:36:14 -0400

    Trees - SearchNode Method

--- 06 - Trees/src/scripts/js/Trees.js
+++ 06 - Trees/src/scripts/js/Trees.js
@@ -44,6 +44,54 @@ class BinarySearchTree {
             }
         }
     }
+
+    searchNode(value) {
+        if(this.root === null) {
+            console.log("Exception: Binary Tree UNDERFLOW");
+            return undefined;
+        }
+        
+        let currentNode = this.root;
+
+        while(true) {
+            if(currentNode.value === value) {
+                return currentNode;
+            }
+
+            if(value < currentNode.value) {
+                //Goes to the left
+                if(currentNode.left !== null) {
+                    currentNode = currentNode.left;
+                }
+                else {
+                    console.log("Node not found");
+                    return undefined;
+                }
+            }
+            else {
+                //Goes to the rigth
+                if(currentNode.rigth !== null) {
+                    currentNode = currentNode.rigth;
+                }
+                else {
+                    console.log("Node not found");
+                    return undefined;
+                }
+            }
+        }
+
+        return currentNode;
+    }
 }
 
 const myBinaryTree = new BinarySearchTree();
+
+myBinaryTree.insert(10);
+
+myBinaryTree.insert(4);
+myBinaryTree.insert(20);
+
+myBinaryTree.insert(2);
+myBinaryTree.insert(8);
+myBinaryTree.insert(17);
+myBinaryTree.insert(170);
